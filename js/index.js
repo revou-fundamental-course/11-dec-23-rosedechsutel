bfec2f47ba9d9d7e39f02ebe139399d460d5eb0e
rosedechsutel <rmdnarmdna@gmail.com>
2023-12-20 12:56:41 +0800

    secondly push

--- js/index.js
+++ js/index.js
@@ -2,27 +2,39 @@ function setName(pname) {
     document.getElementById('nama-orang').innerHTML = pname;
 }
 
-let namaorang = prompt("Halo, siapa nama anda?", "")
+let namaorang = prompt("Halo, You Can Type your name below", "")
 setName(namaorang);
 
-function validateForm() {
-    const name = document.forms["form-container"]["name"].value;
-    const date = document.forms["form-container"]["date"].value;
-    const gender = document.forms["form-container"]["gender"].value;
-    const messages = document.forms["form-container"]["message"].value;
+document.getElementById("form-container").addEventListener("send", function(event){
+    event.preventDefault();
+    var form = this;
+    var name = document.getElementById("name").value;
+    var date = document.getElementById("date").value;
+    var gender = document.getElementById("gender").value;
+    var message = document.getElementById("message").value;
 
-    if (name == "" || date == "" || gender == "" || message == "") {
-        alert("Tidak boleh ada yang kosong");
+    if(name === "" || name === "") {
+        alert("Both fields are required.");
         return false;
     }
-        
-    setSenderUI(name, date, gender, messages);
-    return false;
-}
 
-function setSenderUI(pname, pdate, pgender, pmessages) {
-    document.getElementById("sender-name").innerHTML = pname;
-    document.getElementById("sender-date").innerHTML = pdate;
-    document.getElementById("sender-gender").innerHTML = pgender;
-    document.getElementById("sender-message").innerHTML = pmessage;
-}
+    var newDiv = document.createElement("div");
+    newDiv.innerHTML = "name: " + name + ", date: " + date, "gender: " + gender, "message: " + message ;
+    document.querySelector(".form-message").appendChild(newDiv);
+
+});
+
+var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("slide-row");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        slides[slideIndex-1].style.display = "table-row";  
+        setTimeout(showSlides, 3000); // Change image every 3 seconds
+    }
